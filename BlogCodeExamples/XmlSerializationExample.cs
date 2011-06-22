e3b3ddf48cd32324fa94c1b9315d8606a7e5a450
Travis Silvers <firestrand@gmail.com>
2011-06-22 08:41:54 -0400

    Initial Commit

---
+++ BlogCodeExamples/XmlSerializationExample.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace BlogCodeExamples
+{
+    public class XmlSerializationExample
+    {
+        public static Person DeserializePersonFromXmlString(string personXml)
+        {
+            Person result;
+            var xmlSerializer = new XmlSerializer(typeof (Person));
+            using(var reader = new StringReader(personXml))
+            {
+                result = xmlSerializer.Deserialize(reader) as Person;
+                if(result == null) throw new SerializationException("Deserialization of Person failed.");
+            }
+            return result;
+        }
+        public static string SerializePersonToXmlString(Person person)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Person));
+
+            using (var stringWriter = new StringWriter())
+            {
+                using (var writer = XmlWriter.Create(stringWriter))
+                {
+                    xmlSerializer.Serialize(writer, person);
+                    return stringWriter.ToString();
+                }
+            }
+        }
+
+        public static string SerializePersonToXmlStringFragment(Person person)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Person));
+
+            var settings = new XmlWriterSettings { OmitXmlDeclaration = true };
+            var ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+
+            using (var stringWriter = new StringWriter())
+            {
+                using (var writer = XmlWriter.Create(stringWriter, settings))
+                {
+                    xmlSerializer.Serialize(writer, person, ns);
+                    return stringWriter.ToString();
+                }
+            }
+        }
+
+        [XmlRoot( Namespace = "")]
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string MiddleInitial { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
+    
---
+++ BlogCodeExamplesTests/XmlSerializationExampleTest.cs
@@ -0,0 +1,47 @@
+﻿using BlogCodeExamples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BlogCodeExamplesTests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for XmlSerializationExampleTest and is intended
+    ///to contain all XmlSerializationExampleTest Unit Tests
+    ///</summary>
+    [TestClass]
+    public class XmlSerializationExampleTest
+    {
+        readonly string _personXmlFragment = "<Person><FirstName>John</FirstName><LastName>Smith</LastName></Person>";
+        readonly XmlSerializationExample.Person _person = new XmlSerializationExample.Person{FirstName = "John", LastName = "Smith"};
+        readonly string _personXml = "<?xml version=\"1.0\" encoding=\"utf-16\"?><Person xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><FirstName>John</FirstName><LastName>Smith</LastName></Person>";
+
+        /// <summary>
+        ///A test for DeserializeXmlFromString
+        ///</summary>
+        [TestMethod]
+        public void DeserializeXmlFromStringTest()
+        {
+            XmlSerializationExample.Person expected = _person;
+            XmlSerializationExample.Person actual = XmlSerializationExample.DeserializeXmlFromString(_personXmlFragment);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName,actual.LastName);
+        }
+        [TestMethod]
+        public void SerializePersonToXmlStringTest()
+        {
+            var expected = _personXml;
+            var actual = XmlSerializationExample.SerializePersonToXmlString(_person);
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void SerializePersonToXmlStringFragmentTest()
+        {
+            var expected = _personXmlFragment;
+            var actual = XmlSerializationExample.SerializePersonToXmlStringFragment(_person);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
