6d30314ac37901e3464d01181d1714cd7fad7e97
Travis Silvers <firestrand@gmail.com>
2011-07-15 12:04:11 -0400

    Added Tanh benchmark

---
+++ BlogBenchmarks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using BlogCodeExamples;
+
+namespace BlogBenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting benchmark of Tanh");
+
+            Random rand = new Random();
+            double[] randValues = new double[100000];
+            for (int i = 0; i < randValues.Length; i++)
+            {
+                randValues[i] = rand.NextDouble();
+            }
+            double temp = 0;
+            var sw = new Stopwatch();
+            sw.Start();
+            foreach (double t in randValues)
+            {
+                temp += Math.Tanh(t);
+            }
+            sw.Stop();
+            Console.WriteLine("Math.Tanh: {0} ticks per Calc",sw.ElapsedTicks);
+            
+            sw.Restart();
+            foreach (double t in randValues)
+            {
+                temp += MathFunctions.Tanh2(t);
+            }
+            sw.Stop();
+            Console.WriteLine("MathFunctions.Tanh2: {0} ticks per Calc", sw.ElapsedTicks);
+
+            sw.Restart();
+            foreach (double t in randValues)
+            {
+                temp += MathFunctions.Tanh1(t);
+            }
+            sw.Stop();
+            Console.WriteLine("MathFunctions.Tanh1: {0} ticks per Calc", sw.ElapsedTicks);
+            
+        }
+    }
+}
---
+++ BlogCodeExamples/MathFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlogCodeExamples
+{
+    public class MathFunctions
+    {
+        //This version 3x faster than Math.Tanh
+        public static double Tanh1(double x)
+        {
+            return 2.0 / (1.0 + Math.Exp(-2.0 * x)) - 1.0;
+        }
+        //This version is an estimate and is 10x faster than Math.Tanh
+        public static double Tanh2(double x)
+        {
+            //This is based on Lambert continued fraction taken to 7 divisions and optimized
+            var x2 = x * x;
+            var ax = (((x2 + 378.0) * x2 + 17325.0) * x2 + 135135.0) * x;
+            var bx = ((28.0 * x2 + 3150.0) * x2 + 62370.0) * x2 + 135135.0;
+            return ax / bx;
+        }
+        public static double Scale(double x, double min, double max, double newMin, double newMax)
+        {
+            return ((x - min) / (max - min)) * (newMax - newMin) + newMin;
+        }
+        public static double Scale(double x, double min, double max)
+        {
+            return Scale(x, min, max, 0, 1);
+        }
+    }
+}
